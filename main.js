0878d906590d4315570482073026874ea5517edc
Deepak agrawal <aadeepak05@gmail.com>
2024-04-26 18:29:07 +0530

    ok
    ok

---
+++ main.js
@@ -0,0 +1,81 @@
+/* FOR SMALL SCREEN SIZE MENU BTN */
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+/* SCROLL FEATURE */
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".about__container .section__header", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".about__container .section__description", {
+  ...scrollRevealOption,
+  delay: 100,
+  interval: 200,
+});
+ScrollReveal().reveal(".about__container img", {
+  ...scrollRevealOption,
+  delay: 700,
+});
+
+ScrollReveal().reveal(".service__container .section__header", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".service__container .section__description", {
+  ...scrollRevealOption,
+  delay: 300,
+});
+ScrollReveal().reveal(".service__card", {
+  duration: 1000,
+  delay: 700,
+  interval: 300,
+});
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+
+ScrollReveal().reveal(".blog__content .section__header", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".blog__content h4", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".blog__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".blog__content .blog__btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+const instagram = document.querySelector(".instagram__flex");
+
+Array.from(instagram.children).forEach((item) => {
+  const duplicateNode = item.cloneNode(true);
+  duplicateNode.setAttribute("aria-hidden", true);
+  instagram.appendChild(duplicateNode);
+});
